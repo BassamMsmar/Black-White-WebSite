cd1dbbdd88eadd30ff33e95c08f5fc1e04d651d1
Bassam Msmar <48624392+BassamMsmar@users.noreply.github.com>
2024-08-18 17:27:13 +0300

    edit script to title

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#typed-text", {
+    strings: ["إنتاج سينمائي", "إعلانات تجارية", "المؤثرات البصرية", "العروض ثلاثية الأبعاد", "أفلام وثائقية", "التلحين والغناء", "الموسيقى التصويرية"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1000,
+    loop: true,
+    showCursor: false
+  });
+  
